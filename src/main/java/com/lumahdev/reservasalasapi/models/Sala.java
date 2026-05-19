9320517a0e603da4b0f82b149ae0fc0ee7c5aecd
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-05-19 18:36:43 -0300

    feat: model Sala

---
+++ src/main/java/com/lumahdev/reservasalasapi/models/Sala.java
@@ -0,0 +1,63 @@
+package com.lumahdev.reservasalasapi.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Sala {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long salaId;
+    private String nome;
+    private int capacidade;
+    private String andar;
+    private String bloco;
+    private StatusSala status;
+
+    public Sala(String nome, int capacidade, String andar, String bloco) {
+        if (capacidade < 1) {
+            throw new IllegalArgumentException("A capacidade deve ser maior que 0.");
+        }
+
+        this.nome = nome;
+        this.capacidade = capacidade;
+        this.andar = andar;
+        this.bloco = bloco;
+        this.status = StatusSala.DISPONIVEL;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public String getAndar() {
+        return andar;
+    }
+
+    public String getBloco() {
+        return bloco;
+    }
+
+    public StatusSala getStatus() {
+        return status;
+    }
+
+    public void setStatusIndisponivel() {
+        this.status = StatusSala.INDISPONIVEL;
+    }
+
+    @Override
+    public String toString() {
+        return "Sala{" +
+                "nome='" + nome + '\'' +
+                ", capacidade=" + capacidade +
+                ", andar='" + andar + '\'' +
+                ", bloco='" + bloco + '\'' +
+                ", ativa=" + status +
+                '}';
+    }
+}
