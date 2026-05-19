b14bea063c2661648e4420e22613b3e0c94aea03
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-05-19 16:50:09 -0300

    feat: spring initializer

---
+++ src/main/java/com/lumahdev/reservasalasapi/ReservasalasapiApplication.java
@@ -0,0 +1,13 @@
+package com.lumahdev.reservasalasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasalasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasalasapiApplication.class, args);
+	}
+
+}
