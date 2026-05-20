305d578a4f14d9a590cef6453e0b459bcbc9862c
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-05-20 17:30:52 -0300

    feat dtocadastrosala constraints

--- src/main/java/com/lumahdev/reservasalasapi/Sala/DtoCadastroSala.java
+++ src/main/java/com/lumahdev/reservasalasapi/Sala/DtoCadastroSala.java
@@ -6,8 +6,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record DtoCadastroSala (
-        @NotBlank String nome,
-        @NotNull @Positive @Min(1) int capacidade,
-        @NotBlank String andar,
-        @NotBlank String bloco
+        @NotBlank(message = "Nome é obrigatório.") String nome,
+        @NotNull(message = "Capacidade é obrigatória.") @Positive(message = "Capacidade deve ser um número positivo.") @Min(value = 1, message = "Capacidade deve ser maior que 1.") int capacidade,
+        @NotBlank(message = "Andar é obrigatório.") String andar,
+        @NotBlank(message = "Bloco é obrigatório.") String bloco
 ) {}
