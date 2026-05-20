57d6e5b376943f6bf2b8625db0380e1d861c6057
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-05-19 22:31:02 -0300

    fix dto show id

--- src/main/java/com/lumahdev/reservasalasapi/Usuario/DtoUsuario.java
+++ src/main/java/com/lumahdev/reservasalasapi/Usuario/DtoUsuario.java
@@ -1,13 +1,15 @@
 package com.lumahdev.reservasalasapi.Usuario;
 
 public record DtoUsuario (
+        Long id,
         String nome,
         String sobrenome,
         String email,
         String telefone
 ) {
-    public DtoUsuario (Usuario usuario ){
+    public DtoUsuario (Usuario usuario) {
         this(
+                usuario.getUsuarioId(),
                 usuario.getNome(),
                 usuario.getSobrenome(),
                 usuario.getEmail(),
