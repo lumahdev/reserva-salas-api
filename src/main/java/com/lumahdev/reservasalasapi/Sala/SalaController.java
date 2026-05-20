c08738a4e1720a450fe9a7732287500c0bd3b8c0
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-05-20 17:36:40 -0300

    feat get all salas

--- src/main/java/com/lumahdev/reservasalasapi/Sala/SalaController.java
+++ src/main/java/com/lumahdev/reservasalasapi/Sala/SalaController.java
@@ -4,9 +4,12 @@ import com.lumahdev.reservasalasapi.Usuario.DtoUsuario;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SalaController {
@@ -18,5 +21,11 @@ public class SalaController {
     public ResponseEntity<DtoSala> cadastrarSala(@RequestBody @Valid DtoCadastroSala dto) {
         Sala sala = service.cadastrarSala(dto);
         return ResponseEntity.ok(new DtoSala(sala));
+    }
+
+    @GetMapping("/salas")
+    public ResponseEntity<List<DtoSala>> listarSalas() {
+        List<DtoSala> salas = service.buscarSalas();
+        return ResponseEntity.ok(salas);
     }
 }
