92ff29ca986fe938d199203889eb50245decd863
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-05-19 21:42:31 -0300

    fix spaces

--- src/main/java/com/lumahdev/reservasalasapi/Usuario/DtoCadastroUsuario.java
+++ src/main/java/com/lumahdev/reservasalasapi/Usuario/DtoCadastroUsuario.java
@@ -5,8 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record DtoCadastroUsuario(
-                        @NotBlank String nome,
-                        @NotBlank String sobrenome,
-                        @NotBlank @Email String email,
-                        @NotBlank @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}")  String telefone) {}
+        @NotBlank String nome,
+        @NotBlank String sobrenome,
+        @NotBlank @Email String email,
+        @NotBlank @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}")  String telefone
+) { }
 
