fd63123b795e6c3848811d688882741be46269a1
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-05-20 18:10:28 -0300

    fix reserva post service

--- src/main/java/com/lumahdev/reservasalasapi/Reserva/DtoCadastroReserva.java
+++ src/main/java/com/lumahdev/reservasalasapi/Reserva/DtoCadastroReserva.java
@@ -7,8 +7,8 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record DtoCadastroReserva (
-        @NotNull(message = "Data de início é obrigatória.") @FutureOrPresent(message = "Data de início não pode estar no passado.") LocalDate dataInicio,
-        @NotNull(message = "Data de fim é obrigatória.") @Future(message = "Data de fim não pode estar no passado.") LocalDate dataFim,
+        @NotNull(message = "Data de início é obrigatória.") @FutureOrPresent(message = "Data de início não pode estar no passado. AAAA-MM-DD") LocalDate dataInicio,
+        @NotNull(message = "Data de fim é obrigatória.") @Future(message = "Data de fim não pode estar no passado. AAAA-MM-DD") LocalDate dataFim,
         @NotNull(message = "Identificação da sala é obrigatória.") Long salaId,
         @NotNull(message = "Identificação do usuário é obrigatória.") Long usuarioId
 ) { }
--- src/main/java/com/lumahdev/reservasalasapi/Reserva/ReservaService.java
+++ src/main/java/com/lumahdev/reservasalasapi/Reserva/ReservaService.java
@@ -1,21 +1,51 @@
 package com.lumahdev.reservasalasapi.Reserva;
 
 import com.lumahdev.reservasalasapi.Excecao;
+import com.lumahdev.reservasalasapi.Sala.SalaRepository;
+import com.lumahdev.reservasalasapi.Usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReservaService {
+
     @Autowired
-    private ReservaRepository repository;
+    private ReservaRepository reservaRepository;
 
-    public void cadastrarReserva(DtoCadastroReserva dto) {
-        boolean jaCadastrada = repository.existsBySalaIdAndDataInicioLessThanEqualAndDataFimGreaterThanEqual(dto.salaId(), dto.dataFim(), dto.dataInicio());
+    @Autowired
+    private SalaRepository salaRepository;
 
-        if(jaCadastrada) {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    private boolean salaExiste(Long id){
+        return salaRepository.existsById(id);
+    }
+
+    private boolean usuarioExiste(Long id){
+        return usuarioRepository.existsById(id);
+    }
+
+    public Reserva cadastrarReserva(DtoCadastroReserva dto) {
+        if (!salaExiste(dto.salaId())) {
+            throw new Excecao("Não existe uma sala com este ID.");
+        }
+        if (!usuarioExiste(dto.usuarioId())) {
+            throw new Excecao("Não existe um usuário com este ID.");
+        }
+        if (dto.dataFim().isBefore(dto.dataInicio())) {
+            throw new Excecao("Data final não pode ser anterior à data inicial.");
+        }
+        boolean salaOcupada = reservaRepository
+                .existsBySalaIdAndDataInicioLessThanEqualAndDataFimGreaterThanEqual(
+                    dto.salaId(),
+                    dto.dataFim(),
+                    dto.dataInicio()
+                );
+        if (salaOcupada) {
             throw new Excecao("Já existe uma reserva para esta sala no período especificado.");
         }
-
-        repository.save(new Reserva(dto));
+        Reserva reserva = new Reserva(dto);
+        return reservaRepository.save(reserva);
     }
 }
