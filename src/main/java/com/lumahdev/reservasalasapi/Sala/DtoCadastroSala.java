0f090b3260f7a38bf8a863bd089084a1c8973688
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-05-20 17:35:05 -0300

    fix dtocadastrosala capacidade wrong

--- src/main/java/com/lumahdev/reservasalasapi/Sala/DtoCadastroSala.java
+++ src/main/java/com/lumahdev/reservasalasapi/Sala/DtoCadastroSala.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Positive;
 
 public record DtoCadastroSala (
         @NotBlank(message = "Nome é obrigatório.") String nome,
-        @NotNull(message = "Capacidade é obrigatória.") @Positive(message = "Capacidade deve ser um número positivo.") @Min(value = 1, message = "Capacidade deve ser maior que 1.") int capacidade,
+        @NotNull(message = "Capacidade é obrigatória.") @Positive(message = "Capacidade deve ser um número positivo.") @Min(value = 1, message = "Capacidade deve ser maior que 1.") Integer capacidade,
         @NotBlank(message = "Andar é obrigatório.") String andar,
         @NotBlank(message = "Bloco é obrigatório.") String bloco
 ) {}
