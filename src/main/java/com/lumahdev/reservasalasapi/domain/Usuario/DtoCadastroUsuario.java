95c70f910d1a71fb7ff9b494d606e100c4c127a5
Lumah Pereira <115045300+lumahdev@users.noreply.github.com>
2026-05-28 11:36:22 -0300

    fix dtocadastrousuario: insert login e senha

--- src/main/java/com/lumahdev/reservasalasapi/domain/Usuario/DtoCadastroUsuario.java
+++ src/main/java/com/lumahdev/reservasalasapi/domain/Usuario/DtoCadastroUsuario.java
@@ -8,6 +8,8 @@ public record DtoCadastroUsuario (
         @NotBlank(message = "Nome é obrigatório.") String nome,
         @NotBlank(message = "Sobrenome é obrigatório.") String sobrenome,
         @NotBlank(message = "E-mail é obrigatório.") @Email(message = "E-mail inválido.") String email,
-        @NotBlank(message = "Telefone é obrigatório.") @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}", message = "Telefone inválido.")  String telefone
+        @NotBlank(message = "Telefone é obrigatório.") @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}", message = "Telefone inválido.")  String telefone,
+        @NotBlank(message = "Login é obrigatório.") String login,
+        @NotBlank(message = "Senha é obrigatória.") String senha
 ) { }
 
